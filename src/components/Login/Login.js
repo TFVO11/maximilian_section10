a9cab7d75cdb278c312820800485a518d05c3439
TFVO11 <cosmoyj@naver.com>
2022-07-05 17:07:58 +0900

    section10_4

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,20 +11,35 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('checking form nalidity!');
+
+      setFormIsValid(
+        enteredEmail.includes('@') && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log('CLEANUP');
+    };
+    //cleanup function: useEffect execute this function when next render. then cleanup fnt make it clear
+  }, [enteredEmail, enteredPassword]);
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
 
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+    // setFormIsValid(
+    //   event.target.value.includes('@') && enteredPassword.trim().length > 6
+    // );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
 
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
+    // setFormIsValid(
+    //   event.target.value.trim().length > 6 && enteredEmail.includes('@')
+    // );
   };
 
   const validateEmailHandler = () => {
@@ -82,3 +97,11 @@ const Login = (props) => {
 };
 
 export default Login;
+
+
+//we use useEffect that is operate on rerendering (button click)
+//so we cancel out setFormIsValid
+//add as dependencies, what  you'r using in this side effect function
+//&& is operate when two things is fullfill one && another one
+//in this component, useEffect is executed after [setFormIsValid, enteredEmail, enteredPassword] is changed
+//setTimeout is delay function 
